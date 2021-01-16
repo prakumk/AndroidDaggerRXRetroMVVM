5adffaaca00c1ae524410ef17de7000cb9b874e0
pradeepkumar <android@qandle.com>
2021-01-16 19:42:20 +0530

    First

---
+++ app/src/main/java/com/di/myapplication/BaseApplication.java
@@ -0,0 +1,14 @@
+package com.di.myapplication;
+
+import com.di.myapplication.di.DaggerAppComponent;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return DaggerAppComponent.builder().application(this).build();
+    }
+}
---
+++ app/src/main/java/com/di/myapplication/di/AppComponent.java
@@ -0,0 +1,26 @@
+package com.di.myapplication.di;
+
+import android.app.Application;
+
+import com.di.myapplication.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(modules = {AndroidSupportInjectionModule.class})
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+
+    }
+
+
+}
